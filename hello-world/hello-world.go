c3f68d3973efd9ab9447fa59199deada04416a8f
FreyGeospatial <jfrey0725@gmail.com>
2025-03-28 20:08:25 -0400

    hello world

---
+++ hello-world/hello-world.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+}
+
+// to run the program, use the following command:
+// go run hello-world.go
+
+// to build the program, use the following command:
+// go build hello-world.go
+// this will create an executable file named hello-world
+// to run the executable file, use the following command:
+// ./hello-world
