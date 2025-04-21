9c46ab2b8056909fbaccebc584badd9fdd1423f5
FreyGeospatial <jfrey0725@gmail.com>
2025-04-20 21:00:28 -0400

    more math

--- math.go
+++ math.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	i1, i2, i3 := 12, 45, 68
@@ -9,8 +12,14 @@ func main() {
 
 	f1, f2, f3 := 23.5, 65.1, 76.3
 	floatSum := f1 + f2 + f3
-	fmt.Println("Float sum:", floatSum)
+	fmt.Println("Float sum:", floatSum) //i float issue with precision
 
 	total := float64(intSum) + floatSum
 	fmt.Println("Total sum:", total)
+
+	round := math.Round(floatSum*100) / 100
+	fmt.Println("Rounded float sum:", round)
+
+	fmt.Println(("The value of pi is: "), math.Pi)
+
 }
