3fa83b43997ee35caf095d4eec4c7bccb51d65eb
FreyGeospatial <jfrey0725@gmail.com>
2025-04-29 22:27:25 -0400

    showing how to reference a function

--- calculator_func.go
+++ calculator_func.go
@@ -2,7 +2,9 @@ package main
 
 // Uncomment this import section to use required Go packages
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -15,11 +17,25 @@ func calculate(value1 string, value2 string) float64 {
 	num1, err1 := strconv.ParseFloat(value1, 64) // convert the string to a float
 	if err1 != nil {
 		fmt.Println("Error converting input to float:", err1)
+		panic("Value1 is not a number")
 	}
 	num2, err2 := strconv.ParseFloat(value2, 64) // convert the string to a float
 	if err2 != nil {
 		fmt.Println("Error converting input to float:", err2)
+		panic("Value2 is not a number")
 	}
 
 	return num1 + num2
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin) // Good practice to use bufio.NewReader for reading input
+	fmt.Print("Enter a number: ")
+	val1, _ := reader.ReadString('\n') // generally better than fmt.Scan or fmt.Scanln, which can be tricky with spaces
+
+	fmt.Print("Enter another number: ")
+	val2, _ := reader.ReadString('\n')
+
+	result := calculate(val1, val2)             // call the calculate function
+	fmt.Printf("The result is: %.2f\n", result) // print the result
 }
