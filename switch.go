a43b92e4e44a03d6b76a2925f8d913b7f79d423f
FreyGeospatial <jfrey0725@gmail.com>
2025-06-01 09:02:17 -0400

    switches

--- switch.go
+++ switch.go
@@ -13,8 +13,7 @@ func main() {
 
 	var result string
 	switch day_number {
-	case 0:
-		result = "It's a Sunday!"
+
 	case 1:
 		result = "It's a Monday!"
 	case 2:
@@ -25,10 +24,8 @@ func main() {
 		result = "It's a Thursday!"
 	case 5:
 		result = "It's a Friday!"
-	case 6:
-		result = "It's a Saturday!"
 	default:
-		result = "Invalid day number!"
+		result = "It's the weekend!"
 	}
 	fmt.Println(result)
 }
