5b2d911d9f03f4bdc5732d3b77d6f5cf004b2671
FreyGeospatial <jfrey0725@gmail.com>
2025-06-01 09:10:22 -0400

    scoping var to switch statement

--- switch.go
+++ switch.go
@@ -28,4 +28,26 @@ func main() {
 		result = "It's the weekend!"
 	}
 	fmt.Println(result)
+
+	// can also declare variable in switch statement (can also do this in if statement).
+	// the advantage is that the variable is scoped to the switch statement only.
+	switch day := weekday.String(); day {
+	case "Sunday":
+		fmt.Println("It's Sunday, time to relax!")
+	case "Monday":
+		fmt.Println("It's Monday, back to work!")
+	case "Tuesday":
+		fmt.Println("It's Tuesday, keep going!")
+	case "Wednesday":
+		fmt.Println("It's Wednesday, we're halfway there!")
+	case "Thursday":
+		fmt.Println("It's Thursday, almost the weekend!")
+	case "Friday":
+		fmt.Println("It's Friday, the weekend is near!")
+	case "Saturday":
+		fmt.Println("It's Saturday, enjoy your day off!")
+	default:
+		fmt.Println("Unknown day, please check your calendar!")
+	}
+	// fmt.Println(day) // Uncommenting this line will cause an error because 'day' is scoped to the switch statement only.
 }
